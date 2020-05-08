a12e20e964e0036f1061565df213fddd05eee98e
龙骑将杨影枫 <catwindboy@hotmail.com>
2020-05-08 19:31:38 +0800

    记录法术 手贱卸载了 xcode，结果把 git 弄没了

--- src/personal/personal.controller.ts
+++ src/personal/personal.controller.ts
@@ -1,14 +1,19 @@
 import { Controller, Get, Param, Put, Body } from '@nestjs/common';
 import { PersonalSpellDto } from './dto/PersonalSpellDto';
+import { PersonalService } from './personal.service';
 
 @Controller('personal')
 export class PersonalController {
+    constructor(
+        private readonly personalService: PersonalService
+    ) { }
     @Get(':id')
     async retirevePersonalSpellsById(@Param('id') id: string) {
         return `查询${id}法术`;
     };
     @Put(':id')
     async upDatePersonalSpellById(@Param('id') id: string, @Body() personalDto: PersonalSpellDto) {
-        return `已为${id}记录法术：${personalDto.name}`
+        return this.personalService.updateSpellsById(id, personalDto);
+
     };
 }
--- src/personal/personal.service.ts
+++ src/personal/personal.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { PersonalSpellDto } from './dto/PersonalSpellDto';
 
 @Injectable()
-export class PersonalService {}
+export class PersonalService {
+
+    async updateSpellsById(id: string, personalDto: PersonalSpellDto) {
+        return `已为${id}记录法术：${personalDto.name}`
+    }
+}
